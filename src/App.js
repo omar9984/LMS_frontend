a4fdfd21f704f73c873d2f688a24f91c45e094be
Mohammed Ibrahim Gaballah <44650320+marait123@users.noreply.github.com>
2021-12-17 18:16:13 +0200

    added the premade code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import App from './components/app';
+import Home from './components/home';
+import Public from './components/public';
+import Account from './components/account';
+import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
+import Signout from './components/auth/signout'
+import RequireAuth from './components/auth/require_auth';
+import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
+
+import '../style/style.scss'
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers)
+const token = localStorage.getItem('auth_jwt_token');
+
+// if we have a token, consider the user to be signed in
+if (token) {
+  store.dispatch({type: AUTH_USER})
 }
-
-export default App;
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter hashType="noslash">
+      <App>
+        <Switch>
+          <Route exact path="/" component= {Home} />
+          <Route path="/public" component= {Public} />
+          <Route path="/account" component= {RequireAuth(Account)} />
+          <Route path="/signin" component= {Signin} />
+          <Route path="/signup" component= {Signup} />
+          <Route path="/signout" component= {Signout} />
+        </Switch>
+      </App>
+    </HashRouter>
+  </Provider>
+  , document.getElementById('root'));
