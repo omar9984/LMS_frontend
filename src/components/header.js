aebb3958f6d89262a9b0c0e33b5c59f50cfea80c
Mohammed Ibrahim Gaballah <44650320+marait123@users.noreply.github.com>
2021-12-20 20:44:17 +0200

    successful routing of signin and signup

--- src/components/header.js
+++ src/components/header.js
@@ -1,58 +1,190 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
-import { NavLink } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { Route } from "react-router-dom";
 
-class Header extends Component {
-    renderSignButton(){
-        if (this.props.authenticated){
-            return (
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/signout">Sign out</NavLink>
-                </li>
-            )
-        }else{
-            return (
-                [
-                    <li className="nav-item" key="1">
-                        <NavLink to="/signin" className="nav-link">Sign in</NavLink>
-                    </li>,
-                    <li className="nav-item" key="2">
-                        <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
-                    </li>
-                ]
-            )
-        }
-    }
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <NavLink className="navbar-brand" to="/">MERN</NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/public">Public</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/account">Account</NavLink>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav">
-                        {this.renderSignButton()}
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+// class NestedLink extends Component {
+//   render() {
+//     return <h1>hello</h1>;
+//   }
+// }
+// function NestedLink({ children }) {
+//   return <Route render={({ history }) => ({ children })} />;
+// }
+// function myLink(props) {
+//   return (
+//     <Route
+//       render={({ history }) => (
+//         <Button
+//           color="inherit"
+//           type="button"
+//           onClick={() => {
+//             history.push("/signin");
+//           }}
+//         >
+//           Login
+//         </Button>
+//       )}
+//     />
+//   );
+// }
+function Header(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+
+          {props.authenticated ? (
+            <Fragment>
+              <Route
+                render={({ history }) => (
+                  <Button
+                    color="inherit"
+                    type="button"
+                    onClick={() => {
+                      history.push("/signout");
+                    }}
+                  >
+                    Logout
+                  </Button>
+                )}
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Route
+                render={({ history }) => (
+                  <Button
+                    color="inherit"
+                    type="button"
+                    onClick={() => {
+                      history.push("/signin");
+                    }}
+                  >
+                    Login
+                  </Button>
+                )}
+              />
+              <Route
+                render={({ history }) => (
+                  <Button
+                    color="inherit"
+                    type="button"
+                    onClick={() => {
+                      history.push("/signup");
+                    }}
+                  >
+                    Sign Up
+                  </Button>
+                )}
+              />
+            </Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-function mapStateToProps({auth}){
-    return {
-        authenticated: auth.authenticated
-    }
+// renderSignButton() {
+//     if (this.props.authenticated) {
+//       return (
+//         <li className="nav-item">
+//           <NavLink className="nav-link" to="/signout">
+//             Sign out
+//           </NavLink>
+//         </li>
+//       );
+//     } else {
+//       return [
+//         <li className="nav-item" key="1">
+//           <NavLink to="/signin" className="nav-link">
+//             Sign in
+//           </NavLink>
+//         </li>,
+//         <li className="nav-item" key="2">
+//           <NavLink to="/signup" className="nav-link">
+//             Sign Up
+//           </NavLink>
+//         </li>,
+//       ];
+//     }
+//   }
+
+// rende1r() {
+//     return (
+//       <nav className="navbar navbar-expand-sm navbar-light bg-light">
+//         <NavLink className="navbar-brand" to="/">
+//           MERN
+//         </NavLink>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav mr-auto">
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/public">
+//                 Public
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/account">
+//                 Account
+//               </NavLink>
+//             </li>
+//           </ul>
+//           <ul className="navbar-nav">{this.renderSignButton()}</ul>
+//         </div>
+//       </nav>
+//     );
+//   }
+
+function mapStateToProps({ auth }) {
+  return {
+    authenticated: auth.authenticated,
+  };
 }
 
-export default connect(mapStateToProps, actions)(Header)
+export default connect(mapStateToProps, actions)(Header);
